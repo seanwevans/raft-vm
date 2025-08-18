d7df7713426f569c1d3ed4e43362f1d2d81d86f2
Sean Evans <sean.w.evans@gmail.com>
2025-08-18 12:31:01 -0400

    Remove unused backend abstraction

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,7 @@ pub use compiler::Compiler;
 pub use runtime::Actor;
 pub use vm::VM;
 
-use crate::vm::{Backend, VmError};
+use crate::vm::VmError;
 
 pub const VERSION: &str = env!("CARGO_PKG_VERSION");
 
@@ -17,6 +17,6 @@ pub async fn run(source: &str) -> Result<(), VmError> {
     let bytecode = Compiler::compile(source)
         .map_err(|e| VmError::from(format!("Compilation Error: {}", e)))?;
 
-    let (mut vm, _tx) = VM::new(bytecode, None, Backend::default());
+    let (mut vm, _tx) = VM::new(bytecode, None);
     vm.run().await
 }
