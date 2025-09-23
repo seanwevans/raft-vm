cf462fc096c48a2616550e11291c5d09ccc5820a
Sean Evans <sean.w.evans@gmail.com>
2025-09-23 15:59:20 -0400

    Track reference counts for actor values

--- src/vm/error.rs
+++ src/vm/error.rs
@@ -32,12 +32,6 @@ pub enum VmError {
     CompilationError(#[from] CompilerError),
 }
 
-impl From<SendError<Value>> for VmError {
-    fn from(err: SendError<Value>) -> Self {
-        VmError::ChannelSend(err.to_string())
-    }
-}
-
 impl From<String> for VmError {
     fn from(value: String) -> Self {
         VmError::Message(value)
