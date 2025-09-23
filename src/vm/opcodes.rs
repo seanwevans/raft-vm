eb080b09271d07bac6e249ac65e144221d079a46
Sean Evans <sean.w.evans@gmail.com>
2025-09-23 15:48:49 -0400

    Remove duplicate SendError conversion

--- src/vm/opcodes.rs
+++ src/vm/opcodes.rs
@@ -151,20 +151,14 @@ impl OpCode {
                 Ok(())
             }
 
-            OpCode::JumpIfFalse(target) => {
-                match execution.stack.pop() {
-                    Some(Value::Boolean(false)) => {
-                        execution.ip = *target;
-                        Ok(())
-                    }
-                    Some(Value::Boolean(true)) => Ok(()),
-                    Some(_) => Err(VmError::TypeMismatch("JumpIfFalse")),
-                    None => Err(VmError::StackUnderflowFor("JumpIfFalse")),
-
+            OpCode::JumpIfFalse(target) => match execution.stack.pop() {
+                Some(Value::Boolean(false)) => {
+                    execution.ip = *target;
+                    Ok(())
                 }
                 Some(Value::Boolean(true)) => Ok(()),
                 Some(_) => Err(VmError::TypeMismatch("JumpIfFalse")),
-                None => Err(VmError::StackUnderflow),
+                None => Err(VmError::StackUnderflowFor("JumpIfFalse")),
             },
             OpCode::Call(addr) => {
                 execution.call_stack.push(execution.ip);
